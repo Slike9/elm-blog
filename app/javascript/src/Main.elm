6a5ff6b66dc8bc7f8a18f774811407c2c4e23a43
Sergey Semyonov <semenov@restream.rt.ru>
2017-10-05 01:24:33 +0400

    Load articles

--- app/javascript/src/Main.elm
+++ app/javascript/src/Main.elm
@@ -1,14 +1,23 @@
 module Main exposing (..)
 
-import Html exposing (Html, h1, text)
+import Html exposing (Html, div, h1, text)
 import Html.Attributes exposing (style)
+import Http
+import Json.Decode
 
 
 -- MODEL
 
 
+type alias Article =
+    { id : Int
+    , title : String
+    , body : String
+    }
+
+
 type alias Model =
-    {}
+    { articles : List Article }
 
 
 
@@ -17,7 +26,7 @@ type alias Model =
 
 init : ( Model, Cmd Message )
 init =
-    ( Model, Cmd.none )
+    ( { articles = [] }, loadArticles )
 
 
 
@@ -25,11 +34,14 @@ init =
 
 
 view : Model -> Html Message
-view model =
+view { articles } =
     -- The inline style is being used for example purposes in order to keep this example simple and
     -- avoid loading additional resources. Use a proper stylesheet when building your own app.
-    h1 [ style [ ( "display", "flex" ), ( "justify-content", "center" ) ] ]
-        [ text "Elm Blog" ]
+    div []
+        [ h1 [ style [ ( "display", "flex" ), ( "justify-content", "center" ) ] ]
+            [ text "Elm Blog" ]
+        , div [] [ text ("ArticlesLoaded count: " ++ (toString (List.length articles))) ]
+        ]
 
 
 
@@ -37,7 +49,29 @@ view model =
 
 
 type Message
-    = None
+    = ArticlesLoaded (Result Http.Error (List Article))
+
+
+loadArticles : Cmd Message
+loadArticles =
+    let
+        request =
+            Http.get "/api/v1/articles" decodeArticles
+    in
+        Http.send ArticlesLoaded request
+
+
+decodeArticle : Json.Decode.Decoder Article
+decodeArticle =
+    Json.Decode.map3 Article
+        (Json.Decode.at [ "id" ] Json.Decode.int)
+        (Json.Decode.at [ "title" ] Json.Decode.string)
+        (Json.Decode.at [ "body" ] Json.Decode.string)
+
+
+decodeArticles : Json.Decode.Decoder (List Article)
+decodeArticles =
+    Json.Decode.list decodeArticle
 
 
 
@@ -46,7 +80,12 @@ type Message
 
 update : Message -> Model -> ( Model, Cmd Message )
 update message model =
-    ( model, Cmd.none )
+    case message of
+        ArticlesLoaded (Ok articles) ->
+            ( { model | articles = articles }, Cmd.none )
+
+        ArticlesLoaded (Err _) ->
+            ( model, Cmd.none )
 
 
 
