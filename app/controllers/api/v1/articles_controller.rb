fd9931723f24d4b48f6f5fbdbf8620b41575d7ad
Sergey Semyonov <semenov@restream.rt.ru>
2017-10-17 23:27:56 +0400

    Add an article update api

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -11,13 +11,29 @@ class Api::V1::ArticlesController < Api::V1::ApplicationController
     if article.save
       render json: article
     else
-      render json: { errors: article.errors }, status: :unprocessable_entity
+      render_model_errors(article)
+    end
+  end
+
+  def update
+    if article.update(article_params)
+      render json: article
+    else
+      render_model_errors(article)
     end
   end
 
   private
 
+  def article
+    @article ||= Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :body)
+  end
+
+  def render_model_errors(model)
+    render json: { errors: model.errors }, status: :unprocessable_entity
   end
 end
--- test/controllers/api/v1/articles_controller_test.rb
+++ test/controllers/api/v1/articles_controller_test.rb
@@ -18,4 +18,15 @@ class Api::V1::ArticlesControllerTest < ActionController::TestCase
     article = Article.first
     assert_equal article_attrs[:title], article.title
   end
+
+  test 'PATCH #update' do
+    article = create :article
+    new_title = generate(:string)
+
+    patch :update, params: { id: article.id, article: { title: new_title } }
+
+    assert_response :success
+    article.reload
+    assert_equal new_title, article.title
+  end
 end
